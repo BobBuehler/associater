20c4f74061c851c4c7d5e3ab468c5a002726b1ac
Robert Buehler <robert.buehler@garmin.com>
2016-05-04 11:47:28 -0500

    StringHasher fix

--- main.cpp
+++ main.cpp
@@ -35,10 +35,11 @@ public:
     int operator()(const string& key)
     {
         int hash = 0;
-        for (int i = 0; i < key.length(); ++i)
+        for (size_t i = 0; i < key.length(); ++i)
         {
             hash += key[i] << (8 * (i % 4));
         }
+        return hash;
     }
 };
 
